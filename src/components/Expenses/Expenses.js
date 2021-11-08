34389a429943382425ccb15c6823c8553b2edf54
Mohit182 <mohitguptatech182@gmail.com>
2021-11-08 10:26:25 +0530

    show message when filteredexpense length is 0

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,10 +11,23 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
-  const filteredExpenses = props.items.filter(expense => {
+  const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
-  
+
+  let expenseContent = <p>No expenses found.</p>;
+
+  if(filteredExpenses.length>0){
+    expenseContent=filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ))
+  }
+
   return (
     <div>
       <Card className="expenses">
@@ -22,14 +35,7 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {expenseContent}
       </Card>
     </div>
   );
